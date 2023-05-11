079f6f57af88c47e5544e5f7fefe23b676be2ad2
dianapoliuga <dankaomg@gmail.com>
2023-05-11 18:46:37 +0300

    add json

--- src/app/Components/clients-table/clients-table.component.ts
+++ src/app/Components/clients-table/clients-table.component.ts
@@ -12,7 +12,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   {id: '#2', name: 'Тарас', birth: '12/02/1999', phoneNumber: '0693457861'},
   {id: '#3', name: 'Андріана', birth: '13/05/1995', phoneNumber: '0983281930'},
   {id: '#4', name: 'Василь', birth: '02/09/2003', phoneNumber: '0972683016'},
-  {id: '#5', name: 'Настя', birth: '06/01/1979', phoneNumber: '0677219470'},
+  {id: '#5', name: 'Анастасія', birth: '08/10/1998', phoneNumber: '0678925600'},
   {id: '#6', name: 'Катя', birth: '12/02/2002', phoneNumber: '0986304629'},
   {id: '#7', name: 'Наталя', birth: '19/02/2002', phoneNumber: '0939536720'},
   {id: "#8", name: 'Артем', birth: '07/09/19', phoneNumber: '0995239178'},
